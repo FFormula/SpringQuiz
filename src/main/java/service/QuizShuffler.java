e2dc8d94422bd729884e483371bafff26db2bffd
Jevgenij <fformula@gmail.com>
2018-12-08 01:31:16 +0200

    Simplify shuffle class

--- src/main/java/service/QuizShuffler.java
+++ src/main/java/service/QuizShuffler.java
@@ -1,48 +1,22 @@
 package service;
 
 import face.IQuiz;
-
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class QuizShuffler {
-    private List<IQuiz> list;
+    private QuizStorage storage;
 
     public QuizShuffler(QuizStorage storage) {
-        this.list = storage.getList();
-    }
-
-    public IQuiz getRandomQuiz() {
-        return list.get(getRandom());
+        this.storage = storage;
     }
 
     public List<IQuiz> getRandomQuizList(int count) {
-        List<IQuiz> randomList = new ArrayList<>();
-
-        if (count >= list.size())
-            return getRandomQuizList();
-
-        for (int j = 0; j < count; j ++)
-            while (true) {
-                IQuiz quiz = getRandomQuiz();
-                if (!randomList.contains(quiz)) {
-                    randomList.add(quiz);
-                    break;
-                }
-            }
-        return randomList;
-    }
-
-    public List<IQuiz> getRandomQuizList() {
-        List<IQuiz> randomList = new ArrayList<>();
-        Collections.copy(randomList, list);
-        Collections.shuffle(randomList);
-        return randomList;
-    }
-
-    private int getRandom() {
-        return (int) (Math.random() * list.size());
+        List<IQuiz> list = storage.getQuizList();
+        if (count > list.size())
+            count = list.size();
+        Collections.shuffle(list);
+        return list.subList(0, count);
     }
 
 }
--- src/main/java/service/QuizStorage.java
+++ src/main/java/service/QuizStorage.java
@@ -3,47 +3,32 @@ package service;
 import face.IQuizParser;
 import face.IQuiz;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class QuizStorage {
-    private BufferedReader reader;
     private IQuizParser parser;
-    private List<IQuiz> list;
+    private String filename;
 
     public QuizStorage(String filename, IQuizParser parser) {
-        setReader(filename);
+        this.filename = filename;
         this.parser = parser;
-        fillQuizList();
     }
 
-    private void setReader(String filename) {
+    private BufferedReader getReader() {
         InputStream stream = getClass().getResourceAsStream(filename);
-        reader = new BufferedReader(new InputStreamReader(stream));
+        return new BufferedReader(new InputStreamReader(stream));
     }
 
-    public List<IQuiz> getList() {
-        return list;
-    }
-
-    private void fillQuizList() {
-        try {
-            list = new ArrayList<>();
+    public List<IQuiz> getQuizList() {
+        try (BufferedReader reader = getReader()) {
+            List<IQuiz> list = new ArrayList<>();
             while (reader.ready())
                 list.add(parser.parse(reader.readLine()));
+            return list;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        // как лучше поступить с обработкой исключения?
-        // пробросить дальше - throws
-        // сгенерировать runtime
-        // проигнорировать и вернуть то, что успело считаться
-        // вывести e.printStackTrace()
-        // создать свою систему исключений
-        // что-то ещё?
     }
 }
